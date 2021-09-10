5ffe96996ac651bca584357215c63e47e4295164
sdadige <90069155+sdadige@users.noreply.github.com>
2021-09-11 02:27:16 +0530

    Create GUIFactory.java

---
+++ GUIFactory.java
@@ -0,0 +1,13 @@
+
+package factories;
+
+import buttons.Button;
+import checkboxes.Checkbox;
+/**
+ * Abstract factory knows about all (abstract) product types.
+ */
+
+public interface GUIFactory {
+    Button createButton();
+    Checkbox createCheckbox();
+}
