16c405572bad0c25da4aaa17d9870c6b8321985e
sdadige <90069155+sdadige@users.noreply.github.com>
2021-09-11 02:38:53 +0530

    Create MacOSButton.java

---
+++ MacOSButton.java
@@ -0,0 +1,12 @@
+package buttons;
+
+
+ // This is a MacOS variant of a button.
+ 
+public class MacOSButton implements Button {
+
+    @Override
+    public void paint() {
+        System.out.println("You have created MacOSButton.");
+    }
+}
